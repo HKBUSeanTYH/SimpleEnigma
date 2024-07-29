45e63d864b38dafa62287fd530f99c2b96b95871
HKBUSeanTYH <seantyh118@gmail.com>
2024-07-29 22:31:21 +0800

    1. refactor plugboard process_mappings method to call static ciphermap method

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,6 @@
 
 int main() {
     Enigma enigma {Rotor::create_rotor("../resources/Rotor_1"), Rotor::create_rotor("../resources/Rotor_2"), Rotor::create_rotor("../resources/Rotor_3"), Reflector::create_reflector("../resources/Reflector_1")};
-    enigma.input_plugs("A:B C:D");
+    enigma.input_plugs("A:B B:A C:D D:C");
     std::cout << enigma;
 }
--- src/Plugboard.cpp
+++ src/Plugboard.cpp
@@ -6,16 +6,5 @@ int Plugboard::map(int idx) {
 
 
 void Plugboard::process_mappings(std::vector<std::string> mappings) {
-    std::vector<std::string> temp;
-    for (auto& map : mappings) {
-        StringUtil::split_into(temp, map, ":");
-        if (temp[0].length() == 1 && temp[1].length() == 1) {
-            int left {char_to_int(temp[0][0])};
-            int right {char_to_int(temp[1][0])};
-            this->cipher_mapping[left] = right;
-            this->cipher_mapping[right] = left;
-        } else {
-            std::cout << "Invalid mapping: " << temp[0] << " to " << temp[1] <<"\n";
-        }        
-    }
+    CipherMap::process_mappings(this, mappings);
 }
