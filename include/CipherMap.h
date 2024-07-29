3f858be4276efa7ba63f8f6b807224a3ae4dd5b1
HKBUSeanTYH <seantyh118@gmail.com>
2024-07-29 22:22:54 +0800

    default wiring in init

--- include/CipherMap.h
+++ include/CipherMap.h
@@ -16,14 +16,15 @@ class CipherMap {
         CipherMap() : cipher_mapping{} {
             default_wiring(cipher_mapping);
         }
-        CipherMap(std::vector<std::string> mappings) { 
-            CipherMap::process_mappings(this, mappings);
+        CipherMap(std::vector<std::string> mappings) {
+            default_wiring(cipher_mapping);
+            process_mappings(this, mappings);
         }
+        
+        std::array<int, 26> cipher_mapping;
 
         //init to setup default indices of plugboard
         static void default_wiring(std::array<int, 26>&);
-        std::array<int, 26> cipher_mapping;
-
         //convert a char to its int representation 0-25 and vice versa
         static int char_to_int(char);
         static char int_to_char(int);
