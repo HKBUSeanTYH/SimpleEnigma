81491024303a86825253827e3427246c08c3c9f1
HKBUSeanTYH <seantyh118@gmail.com>
2024-07-31 22:31:23 +0800

    reconfiguring CMakeLists to copy over resources file to binaryDir

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,10 @@
 #include "Enigma.h"
 
 int main() {
-    Rotor one {Rotor::create_rotor("../resources/Rotor_1")};
-    Rotor two {Rotor::create_rotor("../resources/Rotor_2")};
-    Rotor three {Rotor::create_rotor("../resources/Rotor_3")};
-    Reflector ref {Reflector::create_reflector("../resources/Reflector_1")};
+    Rotor one {Rotor::create_rotor("./resources/Rotor_1")};
+    Rotor two {Rotor::create_rotor("./resources/Rotor_2")};
+    Rotor three {Rotor::create_rotor("./resources/Rotor_3")};
+    Reflector ref {Reflector::create_reflector("./resources/Reflector_1")};
 
     Enigma enigma {one, two, three, ref};
     enigma.input_plugs("A:B B:A C:D D:C");
