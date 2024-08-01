420c1964edc700955ad195f22618e4742c15d300
HKBUSeanTYH <seantyh118@gmail.com>
2024-08-01 22:12:39 +0800

    1. moving implementation out of Plugboard.h 2. removing "pessimizing" destructor?

--- include/Plugboard.h
+++ include/Plugboard.h
@@ -6,19 +6,6 @@ class Plugboard : public CipherMap{
     public:
         int map(int) override;
         void process_mappings(std::vector<std::string>);
-        ~Plugboard() {}
-
-        friend std::ostream& operator<<(std::ostream& o, Plugboard const& a) {
-            for (int idx = 0; idx < a.cipher_mapping.size(); ++idx) {
-                int map_val = a.cipher_mapping[idx];
-                if (map_val != idx) {
-                    char cIdx = StringUtil::int_to_char(idx);
-                    char cVal = StringUtil::int_to_char(map_val);
-                    
-                    o << cIdx << " is mapped to " << cVal << "\n";
-                }
-            }
-            return o;
-        }
+        friend std::ostream& operator<<(std::ostream&, Plugboard const&);
 };
 #endif
--- src/Plugboard.cpp
+++ src/Plugboard.cpp
@@ -4,7 +4,19 @@ int Plugboard::map(int idx) {
     return this->cipher_mapping[idx];
 }
 
-
 void Plugboard::process_mappings(std::vector<std::string> mappings) {
     CipherMap::process_mappings(this, mappings);
+}
+
+std::ostream& operator<<(std::ostream& o, Plugboard const& a) {
+    for (int idx = 0; idx < a.cipher_mapping.size(); ++idx) {
+        int map_val = a.cipher_mapping[idx];
+        if (map_val != idx) {
+            char cIdx = StringUtil::int_to_char(idx);
+            char cVal = StringUtil::int_to_char(map_val);
+            
+            o << cIdx << " is mapped to " << cVal << "\n";
+        }
+    }
+    return o;
 }
